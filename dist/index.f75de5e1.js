ed19bc3959e83170cf045195a5eba211de88aae0
farildev <memmedovfaril@gmail.com>
2022-09-20 22:45:07 +0400

    v1.0

--- dist/index.f75de5e1.js
+++
@@ -1,3 +0,0 @@
-console.log("Salam");
-
-//# sourceMappingURL=index.f75de5e1.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
-console.log('Salam');
+const blogSection = document.querySelector('.blogs__content');
+const menuOpen = document.getElementById('menu-btn');
+const menuClose = document.querySelector('.close-btn');
+const navMenu = document.querySelector('.nav__menu')
+
+menuOpen.addEventListener('click', function () {
+    menuOpen.classList.toggle('is-active');
+    navMenu.classList.toggle('is-active');
+})
+
+menuClose.addEventListener('click', function() {
+    navMenu.classList.remove('is-active');
+    menuOpen.classList.remove('is-active');
+})
+
+
+
+
+
+function getData() {
+    let api = `http://localhost:5000/api/blog`;
+    fetch(api)
+    .then (response => response.json())
+    .then (data => {
+        let result = "";
+        if(data[0].Id){
+            data.array.forEach(array => {
+                result += `
+                <div class="blog__zone">
+                <img src="./img/${data.image}" alt="">
+                <p>${data.name}</p>
+            </div>
+                `
+            });
+        }
+    })
+}
