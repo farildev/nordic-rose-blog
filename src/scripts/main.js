2add028775a3a07f90c82b5af35b74b13488e970
farildev <memmedovfaril@gmail.com>
2022-09-24 15:47:56 +0400

    v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,24 +11,3 @@ menuClose.addEventListener('click', function() {
     navMenu.classList.remove('is-active');
     menuOpen.classList.remove('is-active');
 })
-
-
-function getData() {
-    let api = `http://localhost:5000/api/blog`;
-    fetch(api)
-    .then (response => response.json())
-    .then (data => {
-        let blogSection = document.querySelector('.blogs__content');
-        if(data){
-            data.forEach( () => {
-                blogSection += `
-                <div class="blog__zone">
-                <img src="./img/${data.image}" alt="">
-                <p>${data.name}</p>
-            </div>
-                `
-            });
-        }
-    })
-}
-getData();
