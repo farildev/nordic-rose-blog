a7aabb26e60e4708b68f35af52439abadc9de468
farildev <memmedovfaril@gmail.com>
2022-10-23 09:04:30 +0400

    v1.0

---
+++ dist/article.9a0bf5a4.js
@@ -0,0 +1,12 @@
+const menuOpen = document.getElementById("menu-btn");
+const menuClose = document.querySelector(".close-btn");
+const navMenu = document.querySelector(".nav__menu");
+menuOpen.addEventListener("click", function() {
+    menuOpen.classList.toggle("is-active");
+    navMenu.classList.toggle("is-active");
+});
+menuClose.addEventListener("click", function() {
+    navMenu.classList.remove("is-active");
+    menuOpen.classList.remove("is-active");
+});
+
