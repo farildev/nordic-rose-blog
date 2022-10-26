e0d9eb2c2d90ebb4bfef0b6f9db45d8d855a0cae
farildev <memmedovfaril@gmail.com>
2022-10-26 18:52:09 +0400

    v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,7 @@ menuClose.addEventListener('click', function() {
     navMenu.classList.remove('is-active');
     menuOpen.classList.remove('is-active');
 })
+
+let apiKey = '923dfc7395eb491c8380f1125cf62b67';
+let apiURL = `https://newsapi.org/v2/everything?q=tesla&from=2022-09-23&sortBy=publishedAt&${apiKey}`;
+
